14310d47e49893d7eb85b8b6365a1422d3606060
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-03-11 00:18:41 +0300

    1

---
+++ first/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Погодное приложение</h1>
+    <p>Узнать погоду в вашем городе</p>
+    <input type="text" placeholder="Введите погоду">
+    <button>Получить погоду</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  padding: 20px;
+  background: #1f0f24;
+  text-align: center;
+  color: #fff;
+}
+.wrapper h1 {
+  margin-top: 50px;
+}
+.wrapper p {
+  margin-top: 20px;
+}
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+.wrapper input:focus {
+  border-bottom-color: #6e2d7d;
+}
+</style>
